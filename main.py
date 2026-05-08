1104c93268fb1bd79794494bdd61ae505cc5d6e5
Merot-tr <merot.music@gmail.com>
2026-05-08 21:45:25 +0300

    kiralama fonksiyonu ve kullanıcı girişi

--- main.py
+++ main.py
@@ -49,3 +49,36 @@ def verileri_kaydet(dosya_adi, veri_listesi):
 verileri_kaydet("kullanicilar.txt", kullanicilar)
 verileri_kaydet("kitaplar.txt", kitaplar)
 
+
+kullanici = input("Kullanıcı adınızı girin: ")
+sifre = input("Şifrenizi girin: ")
+sifre_kontrol = False
+for k in kullanicilar:
+    if k.kullanici_adi == kullanici and k.sifre == sifre:
+        print(f"Hoş geldiniz, {k.kullanici_adi}!")
+        sifre_kontrol = True
+        break
+
+if sifre_kontrol == False:
+    print("Hatalı kullanıcı adı veya şifre!")
+
+kitap = input("Kiralamak istediğiniz kitabın ISBN numarasını girin: ")
+
+
+def kirala(kullanici_adi, isbn):
+    for kitap in kitaplar:
+        if kitap.isbn == isbn:
+            if kitap.durum == "Mevcut":
+                kitap.durum = "Kirada"
+                kitap.kiralayan = kullanici_adi
+                print(f"{kitap.kitap_adi} kitabı {kullanici_adi} tarafından kiralandı.")
+                file = open("kitaplar.txt", "w", encoding="utf-8")
+                for k in kitaplar:
+                    file.write(str(k) + "\n")
+                    file.close()
+                return
+            else:
+                print(f"{kitap.kitap_adi} kitabı şu anda kirada.")
+                return
+    print("Kitap bulunamadı.")
+
